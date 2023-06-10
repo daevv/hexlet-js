0114ac2ddb06551340d42feb59bcf3985b911a26
daevv <dediaev.danil130702@gmail.com>
2023-06-10 20:35:55 +0300

    add eslint

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-console.log(_.last(["one", "two"]));
+console.log(_.last(['one', 'two']));
 
-console.log("hello world");
+console.log('hello world');
